f512c70a9dad250b3b1618528b2e7979b70b58bf
klhauser <klhauser@epages.de>
2017-02-08 17:48:57 +0100

    showcase for https://github.com/vaadin/framework/issues/8443

--- src/main/java/com/example/VaadinUI.java
+++ src/main/java/com/example/VaadinUI.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
 
@@ -12,11 +15,35 @@ import com.vaadin.ui.themes.ValoTheme;
  */
 @SpringUI
 @Theme(ValoTheme.THEME_NAME)
+@Push
 public class VaadinUI extends UI {
+
+    private HorizontalLayout layout = new HorizontalLayout();
 
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Button("Test", e -> e.getButton().setCaption("Ok")));
+        layout.addComponent(new Button("Test", e -> e.getButton().setCaption("Ok")));
+        setContent(layout);
+
+        buildPushSomething();
+    }
+
+
+    private void buildPushSomething() {
+        class FeederThread extends Thread {
+            @Override
+            public void run() {
+                try {
+                    getUI().access(() -> layout.addComponent(new Label("Hallo")));
+                    Thread.sleep(10000);
+                    getUI().access(() -> layout.addComponent(new Label("Du")));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        new FeederThread().start();
     }
 
 }
